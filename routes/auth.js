3b3199cea6036eaa3118f3af8588cb54be401628
Alphacode18 <skharban@purdue.edu>
2020-07-29 17:04:17 +0530

    Routes Added

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+
+/*
+ * @route   GET /api/auth
+ * @desc    Get a logged in user
+ * @access  Private
+ */
+
+router.get('/', (req, res, next) => {
+  res.send('Get logged in user');
+});
+
+/*
+ * @route   POST /api/auth
+ * @desc    Auth user and get token
+ * @access  Public
+ */
+
+router.post('/', (req, res, next) => {
+  res.send('Login User');
+});
+
+module.exports = router;
