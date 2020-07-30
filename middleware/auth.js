a38f767459e403335c9967a9fb08a3a6fca2b254
Alphacode18 <skharban@purdue.edu>
2020-07-30 16:11:47 +0530

    Authentication Added

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({ message: 'No Token Authorization.' });
+  }
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Token Not Valid' });
+  }
+};
