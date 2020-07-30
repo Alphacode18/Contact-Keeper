7b1c65472a1234973d46c7ea05c0031a3392d627
Alphacode18 <skharban@purdue.edu>
2020-07-30 13:26:18 +0530

    Password Hashing Done

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
@@ -20,12 +21,25 @@ router.post(
       'Please enter password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res, next) => {
+  async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
-    } else {
-      res.send('passed');
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email: email });
+      if (user) {
+        return res.stats(400).json({ message: 'User already exists' });
+      }
+      user = new User({ name, email, password });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send('User Saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
     }
   }
 );
