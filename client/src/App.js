f3d30dbf198ede94a0fb0dad6416b3a5e0a52e3a
Alphacode18 <skharban@purdue.edu>
2020-07-31 08:48:11 +0530

    Clean Up

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className='App'>
       <h1>Welcome To Contact Keeper</h1>
     </div>
   );
-}
+};
 
 export default App;
