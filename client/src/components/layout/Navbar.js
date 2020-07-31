096cb9954bcb87f507fb4e7335e9f84561a49bdf
Alphacode18 <skharban@purdue.edu>
2020-07-31 09:11:23 +0530

    Navigation, Home and About Added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {};
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <Link to='/'>
+          <i className={icon} /> {title}
+        </Link>
+      </h1>
+      <div>
+        <Link to='/about'>About</Link>
+      </div>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'ContactKeeper',
+  icon: 'fas fa-id-card-alt',
+};
 
 export default Navbar;
